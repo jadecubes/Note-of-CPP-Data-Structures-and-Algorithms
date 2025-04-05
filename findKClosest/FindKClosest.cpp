9dcc4a93b89338d5fa1033e31980c5a7e1b80591
Debra <debra_juan@moneynet.tw>
2025-04-05 21:54:27 +0800

    add find k closest

---
+++ findKClosest/FindKClosest.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+class FindKClosest
+{
+public:
+    std::vector<int> findClosestElements(std::vector<int> &arr, int k, int x)
+    {
+        int n = arr.size();
+        int l = 0, h = n - k;
+
+        while (l < h)
+        {
+            int m = (l + h) / 2;
+            if (x - arr[m] > arr[m + k] - x)
+                l = m + 1;
+            else
+                h = m;
+        }
+
+        return std::vector<int>(arr.begin() + l, arr.begin() + l + k);
+    }
+};
